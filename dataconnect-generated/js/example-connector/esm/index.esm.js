00ad709e9670bc8b8d2db3d03c833380ba92bc84
renkunji123 <quocthai312004@thai312004gmail.12004gmailcom>
2025-08-25 11:35:46 +0700

    Base Project

---
+++ dataconnect-generated/js/example-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'example',
+  service: 'trello-app',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/example-connector/index.cjs.js
@@ -0,0 +1,7 @@
+
+const connectorConfig = {
+  connector: 'example',
+  service: 'trello-app',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
